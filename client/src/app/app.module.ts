b7e6ca9ebe4f514886d5de9b8641c25170ebef54
Dmitri Komarovski <Dmitri.Komarovski@gmail.com>
2021-05-15 15:17:29 +0300

    Creating the header component

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
